05d0dfc86f3fd16230bfaeac960c591bd5ead29b
Zucchini10 <ryan.lim.2024@computing.smu.edu.sg>
2025-03-10 14:48:34 +0800

    Create deck.java

---
+++ deck.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+
+public class Deck {
+    private List<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+
+        String[] colors = {"RED", "BLUE", "GREEN", "YELLOW", "PURPLE", "ORANGE"};
+            for (String color : colors){
+                for (int i = 0;i<11;i++){
+                    Card newcard = new Card(color,i);
+                    Deck.add(newcard);
+                }
+            }
+            shuffle();
+        }
+
+    
+    public void shuffle() {
+        Collections.shuffle(cards);
+
+    }
+
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public boolean isEmpty(){
+        if (cards.size()==0){
+            return true;
+        }
+        return false;
+    }
+
+    public int getNumberOfRemainingCards(){ 
+        return cards.size();;
+    }
+    public void drawcard(){
+        cards.remove(0);
+    }
+}
+
