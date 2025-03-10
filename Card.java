05ef611330bf16de6300c271696193e2b78d2cda
CodeIsLife-h <jared.yeo.2024@computing.smu.edu.sg>
2025-03-10 14:51:55 +0800

    card playercardpile
    dasd

---
+++ Card.java
@@ -0,0 +1,30 @@
+public class Card {
+    private String colour;
+    private int value;
+    // private boolean faceUp;
+
+    public Card(String colour, int value, boolean faceUp){
+        this.colour;
+        this.value = value;
+        // this.faceUp = faceUp;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+    public int getValue() {
+        return value;
+    }
+
+    // public boolean isFaceUp() {
+    //     return faceUp;
+    // }
+
+    public String toString(){
+        return colour + "-" + value;
+    }
+
+   
+
+    
+}
---
+++ PlayerCardPile.java
@@ -0,0 +1,27 @@
+public class PlayerCardPile {
+    List<Card> stack;
+
+    public List<Card> getStack() {
+        return stack;
+    }
+
+    public int calcualteStackScore(){
+        int sum = 0;
+        for(Card c : stack){
+            sum += c.getValue();
+        }
+        return sum;
+    } 
+
+    public String toString(){
+        String s = "";
+        for(Card c : stack){
+            s += c.toString() + " ";
+        }
+        return s.substring(0, s.length()-1);
+    }
+
+
+
+    
+}
