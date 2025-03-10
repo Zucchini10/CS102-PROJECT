4ee1ce96492ccf0e7de7f7c61b96df1829832297
le-shen23 <leshen.tan.2024@computing.smu.edu.sg>
2025-03-10 22:39:44 +0800

    aiPlayer.java

---
+++ aiPlayer.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class aiPlayer extends Player {
+    public aiPlayer(String name) {
+        super(name);
+        setAI(true);
+    }
+
+    public Card calculateBestMove(Parade parade) {
+        int paradeSize = parade.getParadeLine().size();
+
+        Card bestCard = hand.get(0);
+        int bestValue = Integer.MAX_VALUE;
+        boolean found = false;
+        for (Card card : hand) {
+            int value = card.getValue();
+
+            if (value >= paradeSize && value < bestValue) {
+                bestValue = value;
+                bestCard = card;
+                found = true;
+            }
+        }
+
+        // pick the smallest card
+        if (!found) {
+            bestCard = hand.get(0);
+            bestValue = bestCard.getValue();
+
+            for (Card card : hand) {
+                int value = card.getValue();
+                if (value < bestValue) {
+                    bestValue = value;
+                    bestCard = card;
+                }
+            }
+        }
+
+        return bestCard;
+    }
+
+    public Card playCard(Card card) {
+        hand.remove(card);
+        return card;
+    }
+}
